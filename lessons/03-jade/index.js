591b7f43cb008058b95815a6bcf929852f066642
Norman Dela Cruz <normandelacruz@celestialhues.com>
2016-01-14 09:29:34 +0800

    Lesson 3 - Jade, added package.json

--- lessons/03-jade/index.js
+++ lessons/03-jade/index.js
@@ -1,15 +1,18 @@
 'use strict';
 
 const app = require('express')();
+const path = require('path');
 
 const port = process.argv[2];
-const jade = process.argv[3];
-
 
 app
-  .set('views', jade)
+  .set('views', path.join(__dirname, 'templates'))
   .set('view engine', 'jade')
   .get('/home', (req, res) => {
-    res.render('index', {date: new Date().toDateString()});
 
-  }).listen(port);
+    res.render('index', {
+      date: new Date().toDateString()
+    });
+
+  })
+  .listen(port);
